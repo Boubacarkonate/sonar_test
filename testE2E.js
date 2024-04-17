6d141b8c4a0b7f015c177cd34ef90c9e3d9d7c20
Bouba <boubacar.konate@outlook.fr>
2024-04-17 15:12:45 +0200

    test complet3

--- testE2E.js
+++ testE2E.js
@@ -1,36 +1,27 @@
-const { Builder, By, Key, until } = require('selenium-webdriver');
+const { Builder, By } = require('selenium-webdriver');
 
-(async function testE2E() {
-    let driver;
-    try {
-        // Initialisation du pilote WebDriver
-        driver = await new Builder().forBrowser('chrome').build();
-        
-        // Navigation vers l'URL de l'application
-        await driver.get("http://localhost:8081/");
+async function example() {
+  let driver = await new Builder().forBrowser('firefox').build();
 
-        // Redimensionnement de la fenêtre du navigateur
-        await driver.manage().window().setRect({ width: 1552, height: 832 });
+  try {
+    // Test steps
+    await driver.get("http://etalentsocial.fr/");
+    await driver.manage().window().setRect({ width: 971, height: 693 });
+    await driver.findElement(By.id("bouton-menu")).click();
+    // await driver.findElement(By.linkText("CANDIDAT")).click();
+    // await driver.findElement(By.id("inputEmail")).click();
+    // await driver.findElement(By.id("inputEmail")).sendKeys("admin@gmail.com");
+    // await driver.findElement(By.id("inputPassword")).click();
+    // await driver.findElement(By.id("inputPassword")).sendKeys("123456");
+    // await driver.findElement(By.css("form")).click();
+    // await driver.findElement(By.css(".btn-dark")).click();
+    // await driver.findElement(By.linkText("Voir les offres d'emploi")).click();
+    // await driver.findElement(By.css(".logo img")).click();
+    // await driver.findElement(By.linkText("déconnexion")).click();
+    console.log('ok');
+  } finally {
+    await driver.quit();
+  }
+}
 
-        // Action : Remplir un champ email
-        await driver.findElement(By.css(".css-view-175oi2r:nth-child(1) > .css-view-175oi2r > .css-textinput-11aywtz")).click();
-        await driver.findElement(By.css(".css-view-175oi2r:nth-child(1) > .css-view-175oi2r > .css-textinput-11aywtz")).sendKeys("toto@gmail.com");
-
-        // Action : Remplir un champ mot de passe
-        await driver.findElement(By.css(".css-view-175oi2r:nth-child(2) > .css-view-175oi2r > .css-textinput-11aywtz")).click();
-        await driver.findElement(By.css(".css-view-175oi2r:nth-child(2) > .css-view-175oi2r > .css-textinput-11aywtz")).sendKeys("123456");
-
-        // Action : Cliquer sur le bouton de connexion
-        await driver.findElement(By.css(".css-view-175oi2r:nth-child(3) .css-text-146c3p1")).click();
-
-        // Ajoutez d'autres actions de test ici...
-
-        console.log("Tests end-to-end terminés avec succès !");
-    } catch (error) {
-        console.error('Une erreur est survenue:', error);
-    } finally {
-        // Fermeture du pilote WebDriver
-        await driver.quit();
-    }
-})();
-
+example();
