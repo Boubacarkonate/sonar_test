5f4189e44259904eb64e693b04f3dcbf80411bea
Bouba <boubacar.konate@outlook.fr>
2024-04-21 12:41:28 +0200

    test + eslint + sonarcloud + build 5

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,5 +1,5 @@
 
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app;
 import { getAuth } from "firebase/auth";
 import {  getFirestore  } from "firebase/firestore";
 import { getStorage, ref } from "firebase/storage";
